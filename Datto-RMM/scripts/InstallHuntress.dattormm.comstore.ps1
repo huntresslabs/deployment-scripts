e59b45ef8909afcdc7b09c383516c4c61f5c6ae4
John Ferrell <jdferrell3@gmail.com>
2020-04-03 01:36:45 -0400

    DattoRMM script - fix protocol check on Win81

--- Datto-RMM/scripts/InstallHuntress.dattormm.comstore.ps1
+++ Datto-RMM/scripts/InstallHuntress.dattormm.comstore.ps1
@@ -217,11 +217,12 @@ function Get-Installer {
 
     # Ensure a secure TLS version is used.
     $ProtocolsSupported = [enum]::GetValues('Net.SecurityProtocolType')
-    if ( ($ProtocolsSupported -contains 'Tls13') -and ($ProtocolsSupported -contains 'Tls12') ){
+    if ( ($ProtocolsSupported -contains 'Tls13') -and ($ProtocolsSupported -contains 'Tls12') ) {
         # Use only TLS 1.3 or 1.2
         LogMessage "Using TLS 1.3 or 1.2..."
-        [Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 12288)
-        [Net.ServicePointManager]::SecurityProtocol += [Enum]::ToObject([Net.SecurityProtocolType], 3072)
+        [Net.ServicePointManager]::SecurityProtocol = (
+            [Enum]::ToObject([Net.SecurityProtocolType], 12288) -bOR [Enum]::ToObject([Net.SecurityProtocolType], 3072)
+        )
     } else {
         LogMessage "Using TLS 1.2..."
         try {
