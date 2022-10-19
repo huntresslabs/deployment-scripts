30d225912995c06748b4cff9b743179eb560339b
Jason Phelps <jason.phelps@huntresslabs.com>
2022-10-19 15:29:17 -0400

    update logging syntax

--- Addigy/InstallHuntress-macOS-Addigy.sh
+++ Addigy/InstallHuntress-macOS-Addigy.sh
@@ -145,7 +145,7 @@ logger "=========== $rmm | Version: $version ==============="
 # VALIDATE OPTIONS PASSED TO SCRIPT
 if [ -z "$organization_key" ]; then
     organizationKey=$(echo "$defaultOrgKey" | xargs)
-    logger "Missing --organization_key parameter, switching to use $defaultOrgKey"
+    logger "--organization_key parameter not present, using defaultOrgKey instead: $defaultOrgKey"
   else
     organizationKey=$(echo "$organization_key" | xargs)
     logger "--organization_key parameter present, set to: $organizationKey"
