e41954ea4469c866d64d45360554957914597102
cameron-huntress <64664216+cameron-huntress@users.noreply.github.com>
2023-05-03 13:45:04 -0500

    Update InstallHuntress.powershellv2.ps1

--- Powershell/InstallHuntress.powershellv2.ps1
+++ Powershell/InstallHuntress.powershellv2.ps1
@@ -70,7 +70,7 @@ $estimatedSpaceNeeded = 200111222
 ##############################################################################
 
 # These are used by the Huntress support team when troubleshooting.
-$ScriptVersion = "Version 2, 2023 May 03, revision 8"
+$ScriptVersion = "Version 2, major revision 7, 2023 May 03"
 $ScriptType = "PowerShell"
 
 # variables used throughout this script
