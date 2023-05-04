7b5ca5b4bce20c8d9e35c5ee28a837c49bc1d347
cameron-huntress <64664216+cameron-huntress@users.noreply.github.com>
2023-05-03 23:28:22 -0500

    Update InstallHuntress.SolarWindsRMM.ps1
    Add revision date

--- SolarwindsRMM/scripts/InstallHuntress.SolarWindsRMM.ps1
+++ SolarwindsRMM/scripts/InstallHuntress.SolarWindsRMM.ps1
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Huntress Labs, Inc.
+# Copyright (c) 2023 Huntress Labs, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -65,7 +65,7 @@ Set-StrictMode -Version Latest
 
 # Do not modify the following variables.
 # These are used by the Huntress support team when troubleshooting.
-$ScriptVersion = "2022 July 29; revision 1"
+$ScriptVersion = "Version 2, major revision 7, 2023 May 1"
 $ScriptType = "SolarWindsRMM"
 
 # Check for an account key specified on the command line.
