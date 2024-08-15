4d843735a41124697c152532ad9d5452e4746200
Alan Bishop [Huntress] <109673875+Alan-Huntress@users.noreply.github.com>
2024-08-15 13:49:41 -0400

    Combine xargs and tr statements
    Co-authored-by: Uttie Gumbula <106617400+uttie-huntress@users.noreply.github.com>

--- Bash/InstallHuntress-macOS-bash.sh
+++ Bash/InstallHuntress-macOS-bash.sh
@@ -143,8 +143,7 @@ logger "=========== $rmm Deployment Script | Version: $version ==============="
 
 # validate options passed to script, remove all invalid characters except spaces are converted to dash
 if [ -z "$organization_key" ]; then
-    organizationKey=$(echo "$defaultOrgKey" | tr -dc '[:alnum:]- ' | tr ' ' '-')
-    organizationKey=$(echo "$organizationKey" | xargs)
+    organizationKey=$(echo "$defaultOrgKey" | tr -dc '[:alnum:]- ' | tr ' ' '-' | xargs)
     logger "--organization_key parameter not present, using defaultOrgKey instead: $defaultOrgKey, formatted to $organizationKey "
   else
     organizationKey=$(echo "$organization_key" | tr -dc '[:alnum:]- ' | tr ' ' '-')
