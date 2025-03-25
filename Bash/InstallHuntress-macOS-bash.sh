5fbd5cfa98f7be5371c255b4268a6d8c4589ce17
stuartjash <stuart.ashenbrenner@gmail.com>
2025-03-25 14:59:43 -0700

    setting default behavior of install_system_extension to true by default instead of false

--- Bash/InstallHuntress-macOS-bash.sh
+++ Bash/InstallHuntress-macOS-bash.sh
@@ -51,11 +51,11 @@ defaultOrgKey="Mac Agents"
 # are being used to deploy the Huntress macOS Agent. Simply replace the text in quotes below.
 rmm="Unspecified RMM"
 
-# Option to install the system extension after the Huntress Agent is installed. In order for this to happen
+# Option to not install the system extension after the Huntress Agent is installed. In order for this to happen
 # without security prompts on the endpoint, permissions need to be applied to the endpoint by an MDM before this script
 # is run. See the following KB article for instructions:
 # https://support.huntress.io/hc/en-us/articles/21286543756947-Instructions-for-the-MDM-Configuration-for-macOS
-install_system_extension=false
+install_system_extension=true
 
 ##############################################################################
 ## Do not modify anything below this line
